0c7ecd81e262001ceb6bf4fe5204639550e04611
ActuallyAJL <zero3260@hotmail.com>
2022-02-21 13:14:03 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,11 @@
+const quarterValue = (arg) => {
+    let quartVal = arg/4;
+    return quartVal;
+}
+
+// Invoke the function and store the return value
+let argu = 200;
+let quarter = quarterValue(argu);
+
+// Log the return value
+console.log(quarter);
